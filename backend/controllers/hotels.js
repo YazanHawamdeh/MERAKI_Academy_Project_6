04d9acc395aaecd178a59f75051b02c62285dacd
MohammedAlayed <alayedmohammed0@gmail.com>
2022-03-07 18:08:14 +0200

    update and delete  functions and routers done

--- backend/controllers/hotels.js
+++ backend/controllers/hotels.js
@@ -13,7 +13,6 @@ const createNewHotels = (req, res) => {
     image5,
     description,
     price,
-    
   ];
   connection.query(query, data, (err, results) => {
     if (err) {
@@ -29,24 +28,59 @@ const createNewHotels = (req, res) => {
     });
   });
 };
+//===========================================
+const getAllHotels = (req, res) => {
+  const query = `SELECT * FROM hotels WHERE is_deleted=0`;
+  connection.query(query, (err, results) => {
+    if (err) {
+      return res.status(500).json({
+        success: false,
+        massage: "server error",
+        err: err,
+      });
+    }
+    res.status(200).json({
+      success: true,
+      results: results,
+    });
+  });
+};
+//===========================================
+const updateHotelsById = (req, res) => {
+  const query = `UPDATE hotels SET ? WHERE id=? AND is_deleted=0`;
+  const body = req.body;
+  const id = req.params.id;
+  const data = [body, id];
 
-const getAllHotels=(req,res)=>{
-    const query = `SELECT * FROM hotels WHERE is_deleted=0`
-    connection.query(query,(err,results)=>{
-        if (err) {
-            return res.status(500).json({
-              success: false,
-              massage: "server error",
-              err: err,
-            });
-          }
-          res.status(200).json({
-            success: true,
-            results: results,
-          })
-    })
-}
-module.exports={
-    createNewHotels,
-    getAllHotels
-}
+  connection.query(query, data, (err, result) => {
+    if (err) {
+      res.json(err);
+    }
+
+    res.json(result);
+  });
+};
+//===========================================
+const deleteHotelsById = (req, res) => {
+  const query = `UPDATE hotels SET is_deleted=1 WHERE id=?`;
+  const id = req.params.id;
+  connection.query(query, id, (err, result) => {
+    if (err) {
+      return res.status(404).json({
+        success: false,
+        massage: `The hotel: ${id} is not found`,
+        err: err,
+      });
+    }
+    res.status(200).json({
+      success: true,
+      massege: `Succeeded to delete hotel with id: ${id}`,
+    });
+  });
+};
+module.exports = {
+  createNewHotels,
+  getAllHotels,
+  updateHotelsById,
+  deleteHotelsById,
+};
--- backend/routes/hotels.js
+++ backend/routes/hotels.js
@@ -2,12 +2,14 @@ const express = require("express");
 
 
 // import
-const { createNewHotels, getAllHotels } = require("../controllers/hotels");
+const { createNewHotels, getAllHotels,updateHotelsById,deleteHotelsById } = require("../controllers/hotels");
 
 //Create Roter
 const hotelsRouter = express.Router();
 
 hotelsRouter.post("/",createNewHotels)
 hotelsRouter.get("/",getAllHotels)
+hotelsRouter.put("/:id",updateHotelsById)
+hotelsRouter.delete("/:id",deleteHotelsById)
 
 module.exports=hotelsRouter
