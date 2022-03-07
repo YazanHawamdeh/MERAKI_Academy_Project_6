959237f238e59c14aaa0ad7eaf72dc541a02f952
MohammedAlayed <alayedmohammed0@gmail.com>
2022-03-07 20:35:46 +0200

    gethotelsBycity

--- backend/controllers/hotels.js
+++ backend/controllers/hotels.js
@@ -1,17 +1,18 @@
 const connection = require("../db/db");
 
 const createNewHotels = (req, res) => {
-  const query = `INSERT INTO hotels (name,image,image2,image3,image4,image5,description,price) VALUES (?,?,?,?,?,?,?,?)`;
-  const { name, image, image2, image3, image4, image5, description, price } =
+  const query = `INSERT INTO hotels (hotelName,image,image2,image3,image4,image5,description,city_id,price) VALUES (?,?,?,?,?,?,?,?,?)`;
+  const { hotelName, image, image2, image3, image4, image5, description,city_id, price } =
     req.body;
   const data = [
-    name,
+    hotelName,
     image,
     image2,
     image3,
     image4,
     image5,
     description,
+    city_id,
     price,
   ];
   connection.query(query, data, (err, results) => {
@@ -48,11 +49,11 @@ const getAllHotels = (req, res) => {
 //===========================================
 const gethotelByName = (req, res) => {
 
-    const query = `SELECT * FROM hotels WHERE name=? AND is_deleted=0`;
-    const hotelName = [req.query.name];
+    const query = `SELECT * FROM hotels WHERE hotelName=? AND is_deleted=0`;
+    const hotel = [req.query.hotelName];
 
 
-    connection.query(query, hotelName, (err, result) => {
+    connection.query(query, hotel, (err, result) => {
         if (err) {
 
             res.json({ success: false, massege: "the hotel not found", err: err })
@@ -80,6 +81,25 @@ const gethotelById = (req, res) => {
         }
         else {
             res.json({ success: true, massege: ` the hotel `, result: result })
+            res.status(200)
+
+        }
+    })
+}
+//===============================================
+const gethotelsBycity = (req, res) => {
+
+    const query = `SELECT  hotels.*,city.name FROM hotels inner Join city on city_id =city.id  WHERE hotels.is_deleted=0 AND city.name=?;`;
+    const cityName =req.params.name
+    connection.query(query,cityName, (err, result) => {
+        if (err) {
+
+            res.json({ success: false, massege: "the hotels not found", err: err })
+            res.status(404)
+
+        }
+        else {
+            res.json({ success: true, massege: ` the hotels `, result: result })
             res.status(200)
 
         }
@@ -126,4 +146,5 @@ module.exports = {
   deleteHotelsById,
   gethotelByName,
   gethotelById,
+  gethotelsBycity,
 };
