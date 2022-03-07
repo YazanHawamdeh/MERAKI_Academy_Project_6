f38f080f66f4e2a166525f910acc0779c9629eee
OmarHamdann <omar.haamdan@gmail.com>
2022-03-07 12:15:02 +0200

    invoke role in server.js

---
+++ backend/routes/roles.js
@@ -0,0 +1,8 @@
+const express=require("express")
+
+//controllers
+const {createNewRole}=require("../controllers/roles");
+const rolesRouter=express.Router()
+rolesRouter.post("/",createNewRole)
+
+module.exports=rolesRouter
