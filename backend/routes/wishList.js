a5a12c9d882af4a87bad13c8eec92ff25c7e11a1
MohammedAlayed <alayedmohammed0@gmail.com>
2022-03-08 11:54:12 +0200

    delete router for delete wishlist

--- backend/routes/wishList.js
+++ backend/routes/wishList.js
@@ -1,11 +1,12 @@
 const express=require("express")
 
-//controllers
-const {addToWishList,GetMyWishList}=require("../controllers/wishList");
+
+const {addToWishList,GetMyWishList,deleteFromMyWishList}=require("../controllers/wishList");
 const authentication = require("../middlewares/authentication");
 
 const wishListRouter=express.Router()
 wishListRouter.post("/:id",authentication,addToWishList)
 wishListRouter.get("/",authentication,GetMyWishList)
+wishListRouter.delete("/:id",deleteFromMyWishList)
 
 module.exports=wishListRouter
