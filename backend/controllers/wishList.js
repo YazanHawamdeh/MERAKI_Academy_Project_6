b5ec55f92d274d408fbdae61ef8ff9cde386cbd2
MohammedAlayed <alayedmohammed0@gmail.com>
2022-03-08 11:51:04 +0200

    GetMyWishList  backend

---
+++ backend/controllers/wishList.js
@@ -0,0 +1,62 @@
+const connection = require("../db/db");
+
+const addToWishList = (req, res) => {
+    const user_id = req.token.userId;
+    const hotel_id = req.params.id;
+    const data = [user_id, hotel_id];
+    const query = `INSERT INTO wishList (user_id, hotel_id) VALUES (?,?)`;
+    connection.query(query, data, (err, result) => {
+      if (err) {
+        return res.status(404).json({
+          success: false,
+          massage: "something went wrong while Adding to wishList",
+          err: err,
+        });
+      }
+  
+      res.status(201).json({
+        success: true,
+        massage: "added successfully ",
+        result: result,
+      });
+    });
+  };
+
+  //====================================
+
+//GetMyWishList
+
+const GetMyWishList = (req, res) => {
+    const query = `SELECT   wishList.*,hotels.name,hotels.image,hotels.description,hotels.price
+  
+    FROM wishList inner Join hotels on hotels_id = hotels.id WHERE wishList.is_deleted=0 AND wishList.user_id=${req.token.userId} ;`;
+  
+    connection.query(query, (err, result) => {
+      if (err) {
+          return res.status(500).json({
+            success: false,
+            massage: "Server Error",
+            err: err,
+          });
+        }
+      if (!result.length) {
+          return res.status(500).json({
+          success: true,
+          massage: "The wishList is empty",
+          
+        });
+      }
+  
+      res.status(200).json({
+        success: true,
+        massage: "All the wishList",
+        results: result,
+      });
+    });
+  };
+
+
+  module.exports={
+    addToWishList,
+    GetMyWishList
+  }
